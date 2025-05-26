068d3ee164140209da790321efa8b9d7d34851b9
Usukh-Erdene Choijamts <uujuu316@gmail.com>
2025-05-26 13:24:21 +0800

    Hotel Models: Hotel and Review (#1595)
    * feat(models): models of User and Booking
    
    Za bugdeeree tusdaa hiisn bgaa bolhoor zurj magad shuu sn shalgaad uurchluh ym baival biy biydee
    heleerei
    
    * feat(models): models of Hotel and Review
    
    :)

---
+++ apps/2CD/hotel/backend/src/models/hotel.ts
@@ -0,0 +1,32 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+export interface IHotel extends Document {
+  hotelName: string;
+  price: number;
+  description: string;
+  phoneNumber: string;
+  amenities: string[];
+  rooms: mongoose.Types.ObjectId[];
+  hotelStar: number;
+  guestReviews: mongoose.Types.ObjectId[];
+  bookings: mongoose.Types.ObjectId[];
+  roomServices: mongoose.Types.ObjectId[];
+}
+
+const HotelSchema: Schema = new Schema(
+  {
+    hotelName: { type: String, required: true },
+    price: { type: Number, required: true },
+    description: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+    amenities: [{ type: String }],
+    rooms: [{ type: Schema.Types.ObjectId, ref: 'Room' }],
+    hotelStar: { type: Number, min: 1, max: 5 },
+    guestReviews: [{ type: Schema.Types.ObjectId, ref: 'Review' }],
+    bookings: [{ type: Schema.Types.ObjectId, ref: 'Booking' }],
+    roomServices: [{ type: Schema.Types.ObjectId, ref: 'RoomService' }],
+  },
+  { timestamps: true }
+);
+
+export const Hotel = mongoose.model<IHotel>('Hotel', HotelSchema);
---
+++ apps/2CD/hotel/backend/src/models/review.ts
@@ -0,0 +1,30 @@
+import mongoose, { Document, Schema } from 'mongoose';
+import { BookingStatus } from './booking';
+export interface IReview extends Document {
+  user: mongoose.Types.ObjectId;
+  hotel: mongoose.Types.ObjectId;
+  comment: string;
+  star: number;
+  room: mongoose.Types.ObjectId[];
+  totalPrice: number;
+  bookStatus: BookingStatus;
+}
+
+const ReviewSchema: Schema = new Schema(
+  {
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    hotel: { type: Schema.Types.ObjectId, ref: 'Hotel', required: true },
+    comment: { type: String, required: true },
+    star: { type: Number, min: 1, max: 5, required: true },
+    room: [{ type: Schema.Types.ObjectId, ref: 'Room' }],
+    totalPrice: { type: Number, required: true },
+    bookStatus: {
+      type: String,
+      enum: ['PENDING', 'CONFIRMED', 'CANCELLED', 'COMPLETED'],
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Review = mongoose.model<IReview>('Review', ReviewSchema);
