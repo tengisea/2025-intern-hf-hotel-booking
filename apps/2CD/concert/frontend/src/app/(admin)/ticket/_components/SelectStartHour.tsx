79e067f7416e570622bf9d424efa83724d5f6013
uskhuu-533 <uskhuunymdavaa9@gmail.com>
2025-06-02 15:39:02 +0800

    feat(concert frontend): component select start hour creating concert … (#1788)
    feat(concert frontend): component select start hour creating concert concert

---
+++ apps/2CD/concert/frontend/src/app/(admin)/ticket/_components/SelectStartHour.tsx
@@ -0,0 +1,46 @@
+/* eslint-disable no-unused-vars */
+import { FormItem, FormLabel } from '@/components/ui/form';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+
+export const SelectStartTime = ({
+  startHour,
+  getHourNumber,
+  setEndHour,
+  setStartHour,
+  hourOptions,
+  endHour,
+}: {
+  getHourNumber: (timeString: string) => number;
+  startHour: string;
+  setStartHour: (value: React.SetStateAction<string>) => void;
+  setEndHour: (value: React.SetStateAction<string>) => void;
+  hourOptions: string[];
+  endHour: string;
+}) => {
+  return (
+    <FormItem data-testid="form-item" className="flex flex-col">
+      <FormLabel data-testid="form-label">эхлэх цаг*</FormLabel>
+      <Select
+        data-testid="select"
+        value={startHour}
+        onValueChange={(value) => {
+          setStartHour(value);
+          if (endHour && getHourNumber(endHour) <= getHourNumber(value)) {
+            setEndHour('');
+          }
+        }}
+      >
+        <SelectTrigger data-testid="select-trigger" className="w-[140px]">
+          <SelectValue data-testid="select-value" placeholder="Эхлэх цаг" />
+        </SelectTrigger>
+        <SelectContent>
+          {hourOptions.map((hour) => (
+            <SelectItem data-testid={`select-hour-${hour}`} key={hour} value={hour}>
+              {hour}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+    </FormItem>
+  );
+};
